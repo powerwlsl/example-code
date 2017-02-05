1f48a6491ad839220dd89f60531f0569a6ec8341
Hyejin Ahn <thebestisyet2com@gmail.com>
2017-02-05 19:34:25 +0900

    Add elastic search on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,46 @@ class Post < ActiveRecord::Base
   belongs_to :company
   belongs_to :publisher, class_name: :User
   has_many :applies
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+def self.search(query)
+    __elasticsearch__.search(
+    {
+      from: 0, size: 500,
+      query: {
+        function_score:{
+          functions: [{
+            gauss: {
+              created_at: {
+                origin: 'now',
+                scale: '3d',
+                offset: '2d',
+                decay: 0.95
+              }
+            }
+          }],
+          query: {
+            multi_match: {
+              query: query,
+              fields: ['title^3', 'description', 'qualification', 'location^3']
+            }
+          }
+        }
+      }
+    }
+    )
+  end
 end
+
+# Delete the previous posts index in Elasticsearch
+Post.__elasticsearch__.client.indices.delete index: Post.index_name rescue nil
+
+# Create the new index with the new mapping
+Post.__elasticsearch__.client.indices.create \
+  index: Post.index_name,
+  body: { settings: Post.settings.to_hash, mappings: Post.mappings.to_hash }
+
+# Index all Post records from the DB to Elasticsearch
+Post.import force: true
